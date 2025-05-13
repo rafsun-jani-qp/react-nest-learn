e08b01406b0ae662659a8286a6797eb20288e95c
rafsun-jani-qp <rafsun.jani@question.com>
2025-05-13 18:25:50 +0600

    Update

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import {TodoListScreen} from './screens/TodoListScreen'
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <TodoListScreen />
+    </>
+  )
+}
---
+++ src/screens/TodoListScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IProps {
+  userId?: string
+}
+
+export const TodoListScreen: React.FC<IProps> = () => {
+  return (
+    <div>
+      <div>TodoListScreen</div>
+    </div>
+  )
+}
